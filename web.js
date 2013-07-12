262ec4491d94605dd2c4f8326a310522ec1e1b02
lsdorothy <lsdorothy@yahoo.com>
2013-07-12 02:15:20 +0000

    Updated web.js - now message reading from index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var data = fs.readFileSync('index.html');
-  resonse.send(data.toString());
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
